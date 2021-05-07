7fb73c3c05c264feccd7a7673af27da382e27aae
Camilo Ortegon <mcortegon@uninorte.edu.co>
2021-05-07 12:34:33 -0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ( Colors.white),
+      appBar: AppBar(
+        backgroundColor: (Colors.blue),
+        title: Center(
+            child: Text("Mi pagina principal")
+        )
+      ),
+      body: Center(
+        child: Text("Hola Mundo"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.ac_unit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      drawer: Drawer(
+        child: Text("drawerContent"),
+      ),
+      endDrawer: Drawer(
+        child: Text("end drawer content"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.add_comment), label: "Chat")
+        ],
+      ),
+      /*
+      persistentFooterButtons: [
+        TextButton(
+          onPressed: () {},
+          child: Text("Pagina de inicio"),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text("Otra pagina"),
+        ),
+      ],
+      */
+    );
+  }
+}
